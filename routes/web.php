2e10ecf59d765f3729ec09129d1e8be823822a69
James Brooks <james@alt-three.com>
2021-03-19 16:36:52 +0000

    wip

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/commit', fn() => shell_exec("git log -1 --pretty=format:'%h - %s (%ci)' --abbrev-commit"));
