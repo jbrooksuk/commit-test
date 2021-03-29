a11826b86de339bf04c30ad06bc50fc0263b539b
James Brooks <james@alt-three.com>
2021-03-29 15:32:57 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('welcome'));
+// Route::get('/', fn() => view('welcome'));
+Route::get('/', fn() => 'Hello World!');
 
 Route::get('/commit', fn() => shell_exec("git log -1 --pretty=format:'%h - %s (%ci)' --abbrev-commit"));
